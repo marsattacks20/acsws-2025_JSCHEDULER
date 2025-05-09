5409e80528f6e2f43e2ff65ec51bb75b42063dc1
marsattacks20 <98043974+marsattacks20@users.noreply.github.com>
2025-05-09 11:33:23 +0200

    Update SchedulerImpl.java

--- jSchedulerComp/src/acsws/SCHEDULER_MODULE/SchedulerImpl/SchedulerImpl.java
+++ jSchedulerComp/src/acsws/SCHEDULER_MODULE/SchedulerImpl/SchedulerImpl.java
@@ -3,83 +3,107 @@ package acsws.SCHEDULER_MODULE.SchedulerImpl;
 //Base component implementation, including container services and component lifecycle infrastructure
 import alma.acs.component.ComponentImplBase;
 import acsws.SCHEDULER_MODULE.SchedulerOperations;
-import acsws.TELESCOPE_MODULE.TelescopeOperations;
-//import acsws.TELESCOPE_MODULE.TelescopeComponentHelper;
-import acsws.STORAGE_MODULE.StorageOperations;
-//import acsws.STORAGE_MODULE.StorageComponentHelper;
-import acsws.DATABASE_MODULE.DataBaseOperations;
+import acsws.DATABASE_MODULE.DataBaseHelper;
+import acsws.TELESCOPE_MODULE.TelescopeHelper;
+import acsws.STORAGE_MODULE.StorageHelper;
+//
+import acsws.TELESCOPE_MODULE.Telescope;
+import acsws.STORAGE_MODULE.Storage;
+import acsws.DATABASE_MODULE.DataBase;
+import alma.acs.component.ComponentLifecycleException;
+import alma.JavaContainerError.wrappers.AcsJContainerServicesEx;
+import acsws.SYSTEMErr.*;
+import acsws.SYSTEMErr.wrappers.AcsJSchedulerAlreadyRunningEx;
+import acsws.SYSTEMErr.wrappers.AcsJSchedulerAlreadyStoppedEx;
+import acsws.SYSTEMErr.wrappers.AcsJNoProposalExecutingEx;
+import alma.acs.container.ContainerServices;
+import alma.acs.component.ComponentLifecycleException;
 //import acsws.DATABASE_MODULE.DataBaseComponentHelper;
 import acsws.TYPES.Proposal;
 import acsws.TYPES.Target;
 import acsws.TYPES.Position;
-
-
-
-
-
-
 import java.util.*;
 //ClassName usually is <Interface>Impl, but can be anything
 public class SchedulerImpl extends ComponentImplBase implements SchedulerOperations {
-    private boolean running = false;
-    final ArrayList<Proposal> proposals = new ArrayList<>();
-    final int i = 0;
-    private boolean stopflag = false;
-    private int propex = -1;
+    private boolean running;
+    private Proposal[] proposals;
+    private boolean stopflag;
+    private int propex;
     private Thread schedulerThread;
-
-
-    public SchedulerImpl() {
-        //By Name
-        Telescope tel = TelescopeComponentHelper.narrow(this.m_containerServices.getComponent("<Name>"));
-        //Release Components
-        this.m_containerServies.releaseComponent(tel.name());
-    }
-
+    private Telescope tel;
+    private Storage storage;
+    private DataBase db;
     @Override
-    public void start (){
-        if (running){ //Exception
+    public void initialize(ContainerServices containerServices) throws ComponentLifecycleException {
+        super.initialize(containerServices);
+        running = false;
+        stopflag = false;
+        propex = -1;
+        schedulerThread = null;
+    }
+    @Override
+    public void execute() throws ComponentLifecycleException{
+        super.execute();
+        try {
+        tel = TelescopeHelper.narrow(this.m_containerServices.getComponent("TELESCOPE"));
+        storage = StorageHelper.narrow(this.m_containerServices.getComponent("STORAGE"));
+        db = DataBaseHelper.narrow(this.m_containerServices.getComponent("DATABASE"));
+        } catch (AcsJContainerServicesEx ex){m_logger.severe("Error in getting components");}
+    }
+    @Override
+    public void cleanUp() {
+        m_containerServices.releaseComponent(tel.name());
+        m_containerServices.releaseComponent(storage.name());
+        m_containerServices.releaseComponent(db.name());
+    }
+    @Override
+    public void start () throws SchedulerAlreadyRunningEx {
+        if (running){ throw new AcsJSchedulerAlreadyRunningEx("Scheduler already started").toSchedulerAlreadyRunningEx();
         }
         schedulerThread = new Thread( () -> {
             try{
+                proposals = db.getProposals();
                 running = true;
                 stopflag = false;
+                m_logger.info("Starting proposal poll");
                 for (Proposal pro : proposals){
                     byte[][] imageList = new byte[1024][1024];
                     propex = pro.pid;
                     int i = 0;
                     for (Target target : pro.targets){
-                        byte[] image = Telescope.observe(target.coordinates, target.expTime);
-                        imageList[i] = image;
-                        Thread.sleep(1000);
+                        try {
+                            m_logger.info("Starting observation");
+                            byte[] image = tel.observe(target.coordinates, target.expTime);
+                            imageList[i] = image;
+                            i++;
+                            Thread.sleep(1000);
+                        } catch (PositionOutOfLimitsEx po)
+                        {m_logger.warning("Position out of limits");}
                     }
+                    m_logger.info("Storing proposal");
                     storage.storeObservation(pro, imageList);
-
                     if (stopflag){
                         break;
                     }
                 }
                 propex = -1;
                 running = false;
-            }catch(InterruptedException e){
-
-            }
+                m_logger.info("All proposal executed");
+            }catch(InterruptedException e){}
         });
         schedulerThread.start();
     }
     @Override
-    public void stop (){
-            if (!stopflag){
-                //EX
+    public void stop () throws SchedulerAlreadyStoppedEx{
+            if (!stopflag){ throw new AcsJSchedulerAlreadyStoppedEx("Scheduler already stopped").toSchedulerAlreadyStoppedEx();
             }
         stopflag = true;
        };
     @Override
-    public int proposalUnderExecution(){
+    public int proposalUnderExecution() throws NoProposalExecutingEx{
         if (propex == -1){
-            //EX
+            throw new AcsJNoProposalExecutingEx("Scheduler already stopped").toNoProposalExecutingEx();
         }
         return propex;
     };
 }
-
